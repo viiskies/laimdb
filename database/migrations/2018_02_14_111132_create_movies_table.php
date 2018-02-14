eb7443dcf6c1c4ee5484c0407c4634be1703130b
Vytautas <viiskis@gmail.com>
2018-02-14 13:25:31 +0200

    movies to categories foreign

---
+++ database/migrations/2018_02_14_111132_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('user_id');
+            $table->text('description');
+            $table->integer('year');
+            $table->double('rating');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
