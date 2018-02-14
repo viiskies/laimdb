fbf952e948754363c889432f3980311cb7d064e6
Vytautas <viiskis@gmail.com>
2018-02-14 12:42:32 +0200

    adds default role as admin when registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'role' => 'admin',
             'password' => bcrypt($data['password']),
         ]);
     }
